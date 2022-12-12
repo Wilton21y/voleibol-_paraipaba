6011e4e44f5633c036e3e3b2621343bed0560f1f
Wilton de Oliveira <104309027+Wilton21y@users.noreply.github.com>
2022-12-12 15:55:53 -0300

    Update Main.js

--- Js/Main.js
+++ Js/Main.js
@@ -1,6 +1,16 @@
+// BOTÃO MENU
+
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) { if (event.type === 'touchstart') event.preventDefault(); const nav = document.getElementById('nav');
   nav.classList.toggle('active'); const active = nav.classList.contains('active');
   event.currentTarget.setAttribute('aria-expanded', active); if (active) { event.currentTarget.setAttribute('aria-label', 'Fechar Menu'); } else { event.currentTarget.setAttribute('aria-label', 'Abrir Menu'); } } btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+// SLIDER 
+function inicia() { document.querySelectorAll('.item').forEach(item => { item.addEventListener('click', (e) => { e.target.scrollIntoView({ inline: 'center', behavior: 'smooth', }); }); }); 
+  
+} 
+
+  window.addEventListener('load', inicia);
+
